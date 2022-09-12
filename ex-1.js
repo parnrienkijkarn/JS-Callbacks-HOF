c4f57a6e78d45dd4bd33eb3ccabb9185a1cba008
Paul Ren <pause.forever@gmail.com>
2022-09-12 13:47:04 +0700

    fix answer ex1

--- ex-1.js
+++ ex-1.js
@@ -1,20 +1,20 @@
-// Exercise #1: For Each Function
-
 function forEach(array, operation) {
-	// Start coding here
-	for (let i=0; i < array.length; i++) {
-		operation(array, i);
-	}
+	for (let i=0; i<array.length; i++) {
+    operation(array, i);
+    newEmployeeSalaries.push(array[i])
+  }
 }
 
-function increaseBy5000(array,  i) {
-	newEmployeeSalaries.push((array[i] + 5000));
+function increase(array, i) {
+    array[i] = array[i] + 5000;
+    return array[i];
 }
 
+  
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
+  
 
-// Using `forEach` function here
+forEach(employeeSalaries, increase);
 
-forEach(employeeSalaries, increaseBy5000)
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
